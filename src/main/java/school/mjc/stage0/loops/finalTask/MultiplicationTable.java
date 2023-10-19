77c6be083e0c12f99ced94dc6216729042f4938a
Alexeykaa <ka.alexey@gmail.com>
2023-10-19 12:28:16 +0300

    init implementation

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,23 @@
 package school.mjc.stage0.loops.finalTask;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter a positive integer: ");
+        String value = br.readLine();
+        try {
+            int i = Integer.parseInt(value);
+            new MultiplicationTable().printTable(i);
+        } catch(NumberFormatException e) {
+            System.err.printf("Invalid format: %s!%n", value);
+        }
+    }
+    public void printTable(int numberTableToPrint) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d%n", i, numberTableToPrint, (i * numberTableToPrint));
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -1,8 +1,28 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
-    public void printPyramid(int cathetusLength) {
+    String expected = """
+                 1
+                212
+               32123
+              4321234
+             543212345
+            65432123456
+            """;
 
+    public void printPyramid(int cathetusLength) {
+        for (int row = 1; row <= cathetusLength; row++) {
+            StringBuilder s = new StringBuilder();
+            s.append(" ".repeat(cathetusLength - row));
+            int width;
+            for (width = row; width > 0; width--) {
+                s.append(width);
+            }
+            for (width += 2; width <= row; width++) {
+                s.append(width);
+            }
+            System.out.println(s);
+        }
     }
 
     public static void main(String[] args) {
